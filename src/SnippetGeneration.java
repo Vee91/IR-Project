def699ac35bd7f56aeb6bf74550181163cb5054d
Sanket <ssnavalakha@gmail.com>
2018-12-09 01:41:01 -0500

    Added Code to highlight Snippet

--- src/SnippetGeneration.java
+++ src/SnippetGeneration.java
@@ -1,8 +1,10 @@
-import java.io.File;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
+
 
 public class SnippetGeneration {
 public static void main(String[] args) throws IOException {
@@ -10,17 +12,66 @@ public static void main(String[] args) throws IOException {
     String query="\"Languages and compilers for parallel processors, especially highly horizontal microcoded machines; code compaction";
     List<String> QueryTerms=Utilities.getQueryTerms(query);
     String[] queryTermArray = new String[QueryTerms.size()];
-    List<String> trisnippetSentences = SnippetGeneration
+    List<String> snippets=new ArrayList<>();
+    snippets = SnippetGeneration
             .generateSnips(QueryTerms.toArray(queryTermArray),"CACM-2112.html",3 );
-    if (trisnippetSentences.size()!=0)
+    if (snippets.size()!=0)
         return;
-    List<String> bisnippetSentences = SnippetGeneration
-            .generateSnips(QueryTerms.toArray(queryTermArray),"CACM-2112.html",2 );
-    if (bisnippetSentences.size()!=0)
-        return;
-    List<String> unnsnippetSentences = SnippetGeneration
-            .generateSnips(QueryTerms.toArray(queryTermArray),"CACM-2112.html",1 );
 
+    snippets = SnippetGeneration
+            .generateSnips(QueryTerms.toArray(queryTermArray),"CACM-2112.html",2 );
+    if (snippets.size()!=0)
+        return;
+    snippets = SnippetGeneration
+            .generateSnips(QueryTerms.toArray(queryTermArray),"CACM-2112.html",1 );
+    if (snippets.size()!=0)
+        return;
+    }
+
+    public static List<String> generateSnippets(String query,String docid) throws IOException {
+        List<String> QueryTerms=Utilities.getQueryTerms(query);
+        String[] queryTermArray = new String[QueryTerms.size()];
+        List<String> snippets=new ArrayList<>();
+        snippets = SnippetGeneration
+                .generateSnips(QueryTerms.toArray(queryTermArray),docid+".html",3 );
+        if (snippets.size()!=0)
+            return snippets;
+
+        snippets = SnippetGeneration
+                .generateSnips(QueryTerms.toArray(queryTermArray),docid+".html",2 );
+        if (snippets.size()!=0)
+            return snippets;
+        snippets = SnippetGeneration
+                .generateSnips(QueryTerms.toArray(queryTermArray),docid+".html",1 );
+        if (snippets.size()!=0)
+            return snippets;
+        return snippets;
+    }
+
+    public static void WriteToHml(List<Query> queries,String resultPath) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter("Results/snipeet.html"));
+        String docContents = new String(Files.readAllBytes(Paths.get(resultPath)));
+        String[] resultArray=docContents.split("\n");
+        writer.write("<!DOCTYPE html>");
+        for (Query query:
+             queries) {
+            writer.write("{Query id = " + query.getQueryId() + " }<br />");
+            for(String docDetails:resultArray) {
+                String[] lineContents=docDetails.split(" ");
+                if(Integer.parseInt(lineContents[0])==query.getQueryId()) {
+                    writer.write("{Doc Name = " + lineContents[2] + " }<br />");
+                    writer.write(" {Snippet} <br />");
+                    List<String> snippets = generateSnippets(query.getQuery(), lineContents[2]+".html");
+                    writer.write(String.join("...", snippets) + "<br />");
+                    writer.write(" {\\Snippet} <br />");
+                    writer.write("{\\Doc Name = " + lineContents[2] + " }<br />");
+                    writer.write("<br \\>");
+                }
+            }
+            writer.write("{/Query}<br />");
+        }
+
+        writer.close();
     }
 
     public static List<String> generateSnips(String[] queryTerms,String fileName,int size) throws IOException {
@@ -53,7 +104,7 @@ public static void main(String[] args) throws IOException {
                         endIndex+=1;
                     endingTerm=docContents.substring(trigramIndex+gram.length(),endIndex);
                 }
-                snipetSentences.add(startingterm+" "+gram+" "+endingTerm);
+                snipetSentences.add(startingterm+" <mark>["+gram+"]</mark> "+endingTerm);
             }
         }
         return snipetSentences;
